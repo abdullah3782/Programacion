523b31a2a3e836263aa6a4fea21d354971d205ac
abdullah3782 <93907645+abdullah3782@users.noreply.github.com>
2022-07-07 19:56:08 +0200

    condicionales_50%completo

--- JavaScript/trabajojs/condicionales1.js
+++ JavaScript/trabajojs/condicionales1.js
@@ -3,7 +3,9 @@ var preciocoche=60000;
 //con prompt hago que el usuario escriba la cantidad
 var dineroAhorrado=prompt("Cuanto tienes ahorrado bro ?");
 //si dineroAhorrado es igual o mayor que el precio de coche saltará el siguiente alert "alert("Puedes comprar tu coche")
-if (dineroAhorrado>=preciocoche) {
+
+var loteria=prompt("Te ha tocado la loteria responde con si o no")
+if (dineroAhorrado>=preciocoche || loteria=="si") {
 alert("Puedes comprar tu coche");
 }
 //si dineroAhorrado es menos que el precio de coche saltará el siguiente alert alert("De momento nada mi pana ahorra money")
