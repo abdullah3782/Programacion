1c39ac693d26a4411fba194d495cc70ece0e53f0
abdullah3782 <93907645+abdullah3782@users.noreply.github.com>
2022-07-16 21:08:58 +0200

    repasoArraysCompletado

--- JavaScript/trabajojs/REPASO/RepasoArrays/repaso.js
+++ JavaScript/trabajojs/REPASO/RepasoArrays/repaso.js
@@ -1,13 +1,47 @@
+//asi se comienza hacer un array
 var articulos=["Zapatillas", "camisetas", "pantalon", "Calcetines"] ;
-
+//eso no es  que nada es un simple texto que queremos que nos aparezca en la web nada más
 document.write ("<p> El primer articulo es <strong>") ;
 //aqui te muestra que array es, ponemos 0 porque empezamos de 0 y no de 1
  document.write (articulos [0] + "</strong> </p>") ;
-
- document.write ("<p> El primer articulo es <strong>") ;
+   //eso no es  que nada es un simple texto que queremos que nos aparezca en la web nada más
+ document.write ("<p> El último articulo es <strong>") ;
+//lenth-1 para que me muestre el ultimo lentgh
  document.write (articulos[articulos.length-1] + "</strong> </p>") ;
+ //unshift es para que agrege un elemto al principio de la array  
  articulos.unshift("Bola")
+   //eso no es  que nada es un simple texto que queremos que nos aparezca en la web nada más
  document.write ("<p> Casi se me olvida !!! Agregamos este elemento: <strong>") ;
+ //para que me escriba el primer elemento de la array 
  document.write (articulos[0] + "</strong> </p>") ;
- articulos.push=prompt("<p> AHORA SI, ELIJELO <strong>")
- document.write(articulos[articulos.length-1] + "</p> </strong>")
+  //eso no es  que nada es un simple texto que queremos que nos aparezca en la web nada más
+ document.write("<p>Vamos a meter un ultimo elemento <strong>")
+ //push para meter un elemento al final de la array 
+ articulos.push (prompt("Pon lo que tu quieras"))
+document.write (articulos[articulos.length-1] + "</p></strong>")
+//para que me escriba el primer elemento de la array y luego las que viene
+document.write ("<p>" + articulos[0] + "<br/>" )
+document.write (articulos[1] + "<br/>")
+document.write (articulos[2] + "<br/>")
+document.write (articulos[3] + "<br/>")
+document.write (articulos[4] + "<br/>")
+document.write (articulos[5] + "<br/> </p>")
+//una simple alerta 
+alert("Voy a borrar el primer elemento")
+//para borrar el primer elemnto
+ articulos.shift();
+ //para que nos ecriba los elemntos que tiene la array dentro de ella en ese momento
+ document.write ("<p>" + articulos[0] + "<br/>")
+ document.write (articulos[1] + "<br/>")
+ document.write (articulos[2] + "<br/>")
+ document.write (articulos[3] + "<br/>")
+ document.write (articulos[4] + "<br/> </p>")
+//una simple alerta 
+ alert("Ahora toca borrar el último elemento")
+ //pop es para borrar el último elemnto 
+ articulos.pop();
+ document.write ("<p>" + articulos[0] + "<br/>")
+ document.write (articulos[1] + "<br/>")
+ document.write (articulos[2] + "<br/>")
+ document.write (articulos[3] + "<br/> </p>")
+ 
--- JavaScript/trabajojs/arrays/arrays.js
+++ JavaScript/trabajojs/arrays/arrays.js
@@ -17,8 +17,33 @@ document.write ("<p> El primer articulo es <strong>") ;
       //eso no es  que nada es un simple texto que queremos que nos aparezca en la web nada más
       document.write ("<p> agregamos un elemento mas al final : <strong>");
       //push es para agregar un elemento al final de la Array y con prompt elije el usuario que elemento quiere ponerle de manera temporal
-        articulos.push (prompt("Intridcuce"));
+        articulos.push (prompt("Introduce"));
         //aqui muestrara el ultimo array y ya que se empieza de 0 le ponemos -1 para que le reste 1 al ultimmo  
         document.write (articulos [articulos.length-1] + "</strong> </p>");
+       //para que me escriba el primer elemento de la array y luego las que viene
+        document.write ("<p>" + articulos[0] + "</br>")
+        document.write (articulos[1] + "</br>")
+        document.write (articulos[2] + "</br>")
+        document.write (articulos[3] + "</br>")
+        document.write (articulos[4] + "</br>")
+        document.write (articulos[5] + "</br> </p>")
+        //una simple alerta 
+        alert("Voy a borrar el primer elemento")
+        // shift para borrar el primer elemnto 
+        articulos.shift();
+        document.write ("<p>" + articulos[0] + "</br>")
+        document.write (articulos[1] + "</br>")
+        document.write (articulos[2] + "</br>")
+        document.write (articulos[3] + "</br>")
+        document.write (articulos[4] + "</br> </p>")
+        //una simple alerta 
+        alert("Ahora toca borrar el último elemento")
+         //pop es para borrar el último elemnto 
+        articulos.pop();
+        document.write ("<p>" + articulos[0] + "</br>")
+        document.write (articulos[1] + "</br>")
+        document.write (articulos[2] + "</br>")
+        document.write (articulos[3] + "</br> </p>")
+    
         
-        
+       
