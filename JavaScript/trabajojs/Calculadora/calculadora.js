7a8e309eb215ee530312c5062ff75e85e794218b
abdullah3782 <93907645+abdullah3782@users.noreply.github.com>
2022-07-27 16:07:08 +0200

    c

--- JavaScript/trabajojs/Calculadora/calculadora.js
+++ JavaScript/trabajojs/Calculadora/calculadora.js
@@ -1,5 +1,9 @@
+var cifras="";
+
 function display_numero (numero) {
 
-document.getElementById("display").value=numero;
+document.getElementById("display").value=cifras+numero;
+cifras=document.getElementById("display").value;
 
 }
+
