315828a6219b16c0b5e5d1843b14d8e1418f73e0
abdullah3782 <93907645+abdullah3782@users.noreply.github.com>
2022-07-27 19:10:43 +0200

    cc

--- JavaScript/trabajojs/Calculadora/calculadora.js
+++ JavaScript/trabajojs/Calculadora/calculadora.js
@@ -1,9 +1,25 @@
 var cifras="";
-
+var acumulado=0
 function display_numero (numero) {
 
 document.getElementById("display").value=cifras+numero;
 cifras=document.getElementById("display").value;
 
+};
+
+function suma () {
+
+acumulado=acumulado+parseInt(cifras)
+document.getElementById("display").value=acumulado
+cifras="";
+
 }
 
+function resta () {
+
+    acumulado=acumulado-cifras
+    document.getElementById("display").value=acumulado
+    cifras="";
+    
+    }
+    
