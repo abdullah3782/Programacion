e234fea7229625abdc43a8152f83c643e264c054
abdullah3782 <93907645+abdullah3782@users.noreply.github.com>
2022-08-04 18:07:22 +0200

    con2_repaso

---
+++ JavaScript/trabajojs/REPASO/repaso_Condicionales/con2.js
@@ -0,0 +1,13 @@
+var dineroAhorrado=prompt("Cuanto dinero tienes?")
+var precioCoche=9000000
+var loteria=prompt("Te ha tocado la loteria")
+if (dineroAhorrado>=precioCoche || loteria=="si") {
+
+alert("puedes comprarte el coche")
+
+}else {
+
+    alert("ahorra dinero")
+}
+
+alert("sigamos con el proceso")
