3ef1a13ed5e29576f98c89ab584dc55d73df828f
abdullah3782 <93907645+abdullah3782@users.noreply.github.com>
2022-08-05 16:28:09 +0200

    repaso_bucles_1

---
+++ JavaScript/trabajojs/REPASO/repaso_Bucle/repaso_BUCLES.js
@@ -0,0 +1,21 @@
+var nombre,edad,poblacion;
+
+do {
+
+nombre=prompt("Pon tu nombre")
+
+}while (!isNaN (nombre));
+
+
+do {
+
+    edad=prompt("Pon tu edad")
+    
+    }while (isNaN (edad));
+
+
+    do {
+
+        poblacion=prompt("Pon tu población")
+        
+        }while (!isNaN (poblacion));
