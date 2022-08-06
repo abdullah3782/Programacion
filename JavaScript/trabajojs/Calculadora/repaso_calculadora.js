88ee6df381498bda97cfc3d4a43e95a0814c15b9
abdullah3782 <93907645+abdullah3782@users.noreply.github.com>
2022-08-06 18:55:01 +0200

    JQuery

---
+++ JavaScript/trabajojs/Calculadora/repaso_calculadora.js
@@ -0,0 +1,14 @@
+var cifras=""
+var suma=false
+var resta=false
+var p_operacion=true
+var acumulado=0
+
+function display_numero (numero) {
+
+document.getElementById("display").value=cifras+numero
+cifras=display_numero+numero
+
+
+
+}
