ddc20a3b140d3f45de9d202d478c1884c841c9de
abdullah3782 <93907645+abdullah3782@users.noreply.github.com>
2022-08-12 00:59:28 +0200

    muchas_cosas

--- JavaScript/trabajojs/JQuery/remov_replace/rem_rep.js
+++ JavaScript/trabajojs/JQuery/remov_replace/rem_rep.js
@@ -3,7 +3,15 @@ $(document).ready(function () {
     //var z=document.getElementById("cerrar");
     //z.addEventListener("click", cerrar_baner, false);
 document.getElementById("cerrar").addEventListener("click", cerrar_baner, false)
-document.getElementById("imagen1").addEventListener("click", carrito, false)
+var imagenes=document.querySelectorAll("#carrete img")
+
+for (var i=0;imagenes.length; i++ ) {
+
+imagenes[i].addEventListener("click", carrito, false)
+
+
+}
+
 });
 
 
@@ -14,9 +22,27 @@ $("#banner").remove();
 
 }
 
-function carrito () {
 
-   $("#imagen1").replaceWith("<div id='imagen11'> Comprado!</div>");
-    
+function carrito (num) {
+
+    if (num.target==imagen1) {
+
+        $("#imagen1").replaceWith("<div id='imagen1'> Agregado al carro </div>");
     
     }
+
+    if (num.target==imagen2) {
+
+        $("#imagen2").replaceWith("<div id='imagen2'> Agregado al carro </div>");
+    
+    }  if (num.target==imagen3) {
+
+        $("#imagen3").replaceWith("<div id='imagen3'> Agregado al carro </div>");
+    
+    }  if (num.target==imagen4) {
+
+        $("#imagen4").replaceWith("<div id='imagen4'> Agregado al carro </div>");
+    
+    }
+
+}
