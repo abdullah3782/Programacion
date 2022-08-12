eaa721c8d4858140ea5dbaae8f5f8707f8fff9d3
abdullah3782 <93907645+abdullah3782@users.noreply.github.com>
2022-08-12 23:55:17 +0200

    repasos

--- JavaScript/trabajojs/REPASO/RepasoArrays/repasooooo.js
+++ JavaScript/trabajojs/REPASO/RepasoArrays/repasooooo.js
@@ -1,48 +1,56 @@
-var articulos=["Zapatillas", "camisetas", "pantalon", "Calcetines"] ;
-document.write ("<p> El primer articulo es <strong>") ;
-document.write ( articulos[0] + "</p> </strong>")
-document.write ("<p> El segundo articulo es <strong>") ;
-document.write ( articulos[1] + "</p></strong>")
-articulos.unshift("BAZA")
-document.write ("<p> El tercer articulo es <strong>") ;
-document.write ( articulos[0] + "</p></strong>")
-articulos.push("Ardilla")
-document.write ("<p> El tercer articulo es <strong>") ;
-document.write ( articulos[articulos.length-1] + "</p></strong>")
-document.write ("<p> El cuarto articulo es <strong>") ;
-document.write ( articulos[5] + "</p></strong>")
-articulos.push(prompt("Que prodcuto quieres añadir?"));
-document.write ("<p> El último articulo es <strong>"+ articulos [articulos.length-1]);
+var articulos=["La casa de papel", "Lupin", "Alba", "BlackList"];
+document.write(" <p> El primer articulo es <strong>")
+document.write( articulos[0] + "</p> </strong>")
+document.write(" <p> El segundo articulo es <strong>")
+document.write( articulos[1] + "</p> </strong>")
+document.write(" <p> El tercero articulo es <strong>")
+document.write( articulos[2] + "</p> </strong>")
+document.write(" <p> El cuarto articulo es <strong>")
+document.write( articulos[3] + "</p> </strong>")
+
+articulos.unshift("Los Ladrones")
+
+document.write(" <p> El quinto articulo es <strong>")
+document.write( articulos[0] + "</p> </strong>")
+
+articulos.push("PEKKA")
+
+document.write(" <p> El último articulo es <strong>")
+document.write (articulos[articulos.length-1] + "</strong> </p>") ;
 
 
-document.write ("<p>" + articulos[0] + "<br/>" )
-document.write (articulos[1] + "<br/>")
-document.write (articulos[2] + "<br/>")
-document.write (articulos[3] + "<br/>")
-document.write (articulos[4] + "<br/>")
-document.write (articulos[5] + "<br/>")
-document.write (articulos[6] + "<br/> </p>")
+articulos.push(prompt("Añade uno tú"))
+document.write(" <p> El articulo escrito por ti es <strong>")
+document.write (articulos[articulos.length-1] + "</strong> </p>") ;
+
+
+document.write( "<p>" + articulos[0] + "<strong>" + "<br>")
+document.write( articulos[1] + "</br>")
+document.write( articulos[2] + "</br>")
+document.write( articulos[3] + "</br>")
+document.write( articulos[4] + "</br>" )
+document.write( articulos[5] + "</br>" )
+document.write( articulos[6] + "</p> </strong>")
+
 
 alert("Voy a borrar el primer elemento")
 
-articulos.shift()
+articulos.shift();
+articulos.shift();
 
-document.write ("<p>" + articulos[0] + "<br/>" )
-document.write (articulos[1] + "<br/>")
-document.write (articulos[2] + "<br/>")
-document.write (articulos[3] + "<br/>")
-document.write (articulos[4] + "<br/>")
-document.write (articulos[5] + "<br/> </p>")
-
-
-alert("Voy a borrar el último elemento")
-
-articulos.pop()
-
-document.write ("<p>" + articulos[0] + "<br/>" )
-document.write (articulos[1] + "<br/>")
-document.write (articulos[2] + "<br/>")
-document.write (articulos[3] + "<br/>")
-document.write (articulos[4] + "<br/> </p>")
+document.write( "<p>" + articulos[0] + "<strong>" + "<br>")
+document.write( articulos[1] + "</br>")
+document.write( articulos[2] + "</br>")
+document.write( articulos[3] + "</br>")
+document.write( articulos[4] + "</br> </p> </strong>" )
 
 
+alert("Voy a borrar el últimmo elemento")
+
+articulos.pop();
+
+
+document.write( "<p>" + articulos[0] + "<strong>" + "<br>")
+document.write( articulos[1] + "</br>")
+document.write( articulos[2] + "</br>")
+document.write( articulos[3] + "</br> </p> </strong>")
