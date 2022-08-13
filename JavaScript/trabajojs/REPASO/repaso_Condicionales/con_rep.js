a5fbeb42085192175999fc6f658e6ce15f37b30a
abdullah3782 <93907645+abdullah3782@users.noreply.github.com>
2022-08-13 15:30:57 +0200

    condicionales_pt1

---
+++ JavaScript/trabajojs/REPASO/repaso_Condicionales/con_rep.js
@@ -0,0 +1,13 @@
+var coche=50000000
+var dineroahorrado=prompt("Cúanto dinero tienes")
+var loteria=prompt("Te tocó la loteria? responde con un si o no  ")
+
+if (dineroahorrado>=coche || loteria=="si") {
+
+alert("Puedes comprarte el coche")
+
+}else {
+
+alert("Lo siento mio")
+
+}
