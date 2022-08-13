3fc83fee2e4ab38d8b64edee2ce0c99c7d85e470
abdullah3782 <93907645+abdullah3782@users.noreply.github.com>
2022-08-13 15:54:09 +0200

    calcu_repasoo

--- JavaScript/trabajojs/REPASO/repaso_Condicionales/calclu_repaso.js
+++ JavaScript/trabajojs/REPASO/repaso_Condicionales/calclu_repaso.js
@@ -1,40 +1,41 @@
 var num1=prompt("Pon el primer dígito")
 var num2=prompt("Pon el segundo dígito")
-var operacion=prompt("Que operación quieres hacer, sumar, restar, multiplicar o dividir")
+var operacion=prompt("Elije la operación, sumar, restar, multiplicar o dividir")
+if (!isNaN(num1) && !isNaN(num2)) {
 
-if (!isNaN (num1) && !isNaN(num2)) {
 
 if (operacion=="sumar") {
 
-alert(parseInt (num1) + parseInt (num2)) 
+alert(parseInt(num1) + parseInt (num2))
 
-} else if (operacion=="restar") {
+}else if (operacion=="restar") {
 
-    alert(parseInt (num1) - parseInt(num2))
+alert(num1-num2)
 
 }else if (operacion=="multiplicar") {
 
-    alert(parseInt (num1) * parseInt(num2))
-
+    alert(num1*num2)
+    
 }else if (operacion=="dividir") {
 
-    alert(parseInt (num1) / parseInt(num2))
-
+    alert(num1/num2)
+    
 }else {
 
-    alert("ERROR 404")
+    alert("ERROR404")
 }
 
 }
 else {
 
-    if (typeof(num1)=="string" || typeof(num2)=="string") {
+if (typeof(num1)=="string" && typeof(num2)=="string") {
 
-        alert("Pon numeros")
-    }
-else { 
-
-alert("Deja de inventar las cosas anda")
+alert("Pon bien los datos anda")
 
 }
+else {
+
+    alert("En los 2 has puesto letra")
+}
+
 }
