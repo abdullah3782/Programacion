96093447a0e93f9a30e8f0c1826f1f6532f38714
abdullah3782 <93907645+abdullah3782@users.noreply.github.com>
2022-08-19 16:52:52 +0200

    repaso_hecho

--- JavaScript/trabajojs/REPASO/repaso_Query/rem_rep.js
+++ JavaScript/trabajojs/REPASO/repaso_Query/rem_rep.js
@@ -1,8 +1,13 @@
 $(document).ready(function () {
     
 var i=document.getElementById("cerrar").addEventListener("click",  quitar_baner, false )
+var imagenes=document.querySelectorAll("#carrete img")
+for (var i=0;imagenes.length; i++ ) {
 
-
+    imagenes[i].addEventListener("click", carrito, false)
+    
+    
+    }
 });
 
 function quitar_baner () {
@@ -15,7 +20,25 @@ function quitar_baner () {
 
 function carrito (num) {
 
+    if (num.target==imagen1) {
 
+        $("#imagen1").replaceWith("<div id='imagen1'> Agregado al carro </div>");
+    
+    }
+
+    if (num.target==imagen2) {
+
+        $("#imagen2").replaceWith("<div id='imagen2'> Agregado al carro </div>");
+    
+    }  if (num.target==imagen3) {
+
+        $("#imagen3").replaceWith("<div id='imagen3'> Agregado al carro </div>");
+    
+    }  if (num.target==imagen4) {
+
+        $("#imagen4").replaceWith("<div id='imagen4'> Agregado al carro </div>");
+    
+    }
 
     
 }
