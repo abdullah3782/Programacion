914b4cc933162031a60a2a969f92b0fcd2ae4f82
abdullah3782 <93907645+abdullah3782@users.noreply.github.com>
2022-09-03 18:20:06 +0200

    disminuye

--- JavaScript/trabajojs/JQuery+CSS/READ_Change_CSS/RCC_EX1.js
+++ JavaScript/trabajojs/JQuery+CSS/READ_Change_CSS/RCC_EX1.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
     document.getElementById("aumentar1").addEventListener("click", aumentar, false)
+    document.getElementById("disminuirr").addEventListener("click", disminuir, false)
+
 });
 
 
@@ -9,7 +11,18 @@ var amm1=$("#noticia1").css("font-size");
 
 amm1=parseInt(amm1)
 
-$("#noticia1").css("font-size", amm1*2);
+$("#noticia1").css("font-size", amm1*1.3);
 
 }
 
+
+function disminuir (){
+
+    var dis1=$("#noticia1").css("font-size");
+    
+    dis1=parseInt(dis1)
+    
+    $("#noticia1").css("font-size", dis1-1.3);
+    
+    }
+    
