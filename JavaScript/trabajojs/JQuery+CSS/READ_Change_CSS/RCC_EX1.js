cc0858ddb65c605d6ea1121910011a6efebf78a2
abdullah3782 <93907645+abdullah3782@users.noreply.github.com>
2022-09-03 18:33:20 +0200

    if_else

--- JavaScript/trabajojs/JQuery+CSS/READ_Change_CSS/RCC_EX1.js
+++ JavaScript/trabajojs/JQuery+CSS/READ_Change_CSS/RCC_EX1.js
@@ -1,28 +1,61 @@
 $(document).ready(function () {
     document.getElementById("aumentar1").addEventListener("click", aumentar, false)
-    document.getElementById("disminuirr").addEventListener("click", disminuir, false)
+    document.getElementById("disminuir1").addEventListener("click", disminuir, false)
 
 });
 
 
-function aumentar (){
+function aumentar(e) {
 
-var amm1=$("#noticia1").css("font-size");
+    var zonaActuacion;
 
-amm1=parseInt(amm1)
+    if (e.target == aumentar1) {
 
-$("#noticia1").css("font-size", amm1*1.3);
+        zonaActuacion = "#noticia1"
+
+    } else if
+
+        (e.target == aumentar2) {
+
+        zonaActuacion = "#noticia2"
+
+    } else if
+
+        (e.target == aumentar3) {
+
+        zonaActuacion = "#noticia3F"
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    var amm1 = $("#noticia1").css("font-size");
+
+    amm1 = parseInt(amm1)
+
+    $("#noticia1").css("font-size", amm1 * 1.3);
 
 }
 
 
-function disminuir (){
+function disminuir() {
 
-    var dis1=$("#noticia1").css("font-size");
-    
-    dis1=parseInt(dis1)
-    
-    $("#noticia1").css("font-size", dis1-1.3);
-    
-    }
-    
+    var dis1 = $("#noticia1").css("font-size");
+
+    dis1 = parseInt(dis1)
+
+    $("#noticia1").css("font-size", dis1 - 1.3);
+
+}
